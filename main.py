67aefe3e82982a2d9c632c475856a81bda164d5e
goku58432 <160702906+goku58432@users.noreply.github.com>
2026-04-19 11:18:33 -0600

    Add Netlify app to CORS allowed origins

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000","https://dulcet-seahorse-3b5188.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
